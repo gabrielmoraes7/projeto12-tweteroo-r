b501cbffd0825554fc85d1b07edd93def66becdb
gabrielmoraes7 <gabriel.moclemente7@gmail.com>
2023-12-17 23:03:30 -0300

    correção, ultima rota feita foi POST, rota POST /tweets finalilzada agora

--- src/App.js
+++ src/App.js
@@ -16,17 +16,32 @@ const tweets = [];
 //POST: /sign-up 
   app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body;
-    users.push
-    (
+    users.push(
       { 
         username,
          avatar
       }
     );
-
-    res.send("Ok!"); 
+    res.send("OK"); 
   }
   );
+
+  // POST /tweets
+  app.post("/tweets", (req, res) => {
+
+    //Corpo do tweet
+    const { username, tweet } = req.body;
+
+    //Verifica se o usuario esta de fato cadastrado varendo a matriz user com o username passado pela body como parametro de referência
+    const userExists = users.find((user) => user.username === username);
+
+    //Caso ele não esteja na matriz dos usuarios é colocado como um acesso não autorizado(UNAUTHORIZED)
+    if (!userExists) return res.send("UNAUTHORIZED");
+    
+    //Posta o tweet
+    tweets.push({ username, tweet });
+    res.send("OK");
+});
 
 //Conexão ao servidor na porta 5000
 const PORT = 5000;
